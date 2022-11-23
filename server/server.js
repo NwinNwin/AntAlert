b38b619b5851f044837fee6968fa363500e90dfc
Steven Zhou <stevenz9@uci.edu>
2022-11-22 20:39:22 -0800

    initialized server folder with nodemailer set up

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const nodeMailer = require("nodemailer");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+async function createMail() {
+  const transporter = nodeMailer.createTransport({
+    service: process.env.SERVICE,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASSWORD,
+    },
+  });
+
+  const options = {
+    from: process.env.HOST,
+    // put user email here
+    to: `${email}`,
+    subject: "Course Spot Opening",
+    // put courseId and courseName of user's desire
+    text: `A new spot has been opened up for your desired course, ${courseId}, ${courseName}!`
+  };
+
+  transporter.sendMail(options, function (err, info) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Sent: " + info.response);
+  });
+}
