c91bf6f79db8cd11627fe5fb8e6013fcfa40964c
NwinNwin <ndnguyen2003@gmail.com>
2022-11-22 19:44:02 -0800

    1st commit - Initilized Client folder

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to AntAlert!</h1>
+        <input type="email" placeholder='email'/>
+        <input type="text" placeholder='class'/>
+        <button>Alert Me!</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
