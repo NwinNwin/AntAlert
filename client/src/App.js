7c728bcf4163e5c03ebde113d84a0b220e9601f1
NwinNwin <ndnguyen2003@gmail.com>
2023-04-03 11:01:49 -0700

    bug

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
-      <InfoGrab />
+      {/* <InfoGrab /> */}
       <AlertForm />
     </div>
   );
